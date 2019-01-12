a1d45ccc49ab3dd7f1bc10a17cc6fabe30ddb111
ptamayo <ptamayo97@yahoo.com>
2019-01-12 04:28:45 -0800

    cards now shuffle when clicked

--- src/App.js
+++ src/App.js
@@ -5,13 +5,42 @@ import Card from "./components/Card";
 import characters from "./characters.json";
 import "./App.css";
 
+// const styles = {
+//   cardBackground: {
+//     backgroundImage: `url(${characters.image})`
+//   }
+// };
+
 class App extends Component {
   state = {
-    characters
+    characters,
+    clickedCharacters: [],
+    score: 0,
+    topScore: 0,
+    message: ""
   };
+
+  cardShuffle = () => {
+    const shuffledCharacters = this.shuffleArray(characters);
+
+    this.setState({ charecters: shuffledCharacters });
+  };
+
+  shuffleArray = charactersArray => {
+    for (let i = charactersArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [charactersArray[i], charactersArray[j]] = [
+        charactersArray[j],
+        charactersArray[i]
+      ];
+    }
+  };
+
+  cardCheck = () => {};
+
   render() {
     return (
-      <div className="App">
+      <div className="App ">
         <div id="nav" className="red z-depth-2 white-text">
           <div>
             <strong>Clicky Game!</strong>
@@ -28,17 +57,17 @@ class App extends Component {
             once!
           </h3>
         </div>
-        <div className="container">
+        <div className="container cards-section">
           <div className="row">
             {this.state.characters.map(character => (
-              <div className="col s12 m3">
-                <Card
-                  key={character.id}
-                  name={character.name}
-                  src={character.image}
-                  alt={character.name}
-                />
-              </div>
+              <Card
+                key={character.id}
+                name={character.name}
+                image={character.image}
+                alt={character.name}
+                shuffle={this.cardShuffle}
+                // style={styles.cardBackground}
+              />
             ))}
           </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Card = props => {
   return (
-    <div className="card" key={props.id} name={props.name}>
-      <div className="card-image">
-        <img src={props.src} alt={props.alt} />
+    <div className="col s12 m3 card-wrapper ">
+      <div
+        className="card card-container hvr-grow z-depth-2"
+        key={props.id}
+        name={props.name}
+        onClick={props.shuffle}
+      >
+        <div className="card-image card-child ">
+          <img src={props.image} alt={props.alt} />
+        </div>
       </div>
     </div>
   );
