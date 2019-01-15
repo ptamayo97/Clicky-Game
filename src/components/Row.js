bd758bc85b77daaabf7c0084443a5839d8ce732c
ptamayo <ptamayo97@yahoo.com>
2019-01-14 21:49:17 -0800

    Updates

--- src/components/Row.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Row = props => {
-  return <div className="row cards-section">{props.children}</div>;
-};
-
-export default Row;
--- src/components/Wrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Wrapper = props => {
-  return <div className="App ">{props.children}</div>;
-};
-
-export default Wrapper;
