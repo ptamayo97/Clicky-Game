115e71ab16d88b8b85de6ca9e1de3aa03db2b5fb
ptamayo <ptamayo97@yahoo.com>
2019-01-15 03:55:26 -0800

    Updates

---
+++ precache-manifest.9b95d73621efdfc6ff2c6d7e49a86631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "2f47bd85c62b387701d2",
+    "url": "/Clicky-Game/static/js/main.2f47bd85.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2f47bd85c62b387701d2",
+    "url": "/Clicky-Game/static/css/main.af2b08fa.chunk.css"
+  },
+  {
+    "revision": "5f68fad6f4474622eaa6ca14e6a3ded7",
+    "url": "/Clicky-Game/index.html"
+  }
+];
